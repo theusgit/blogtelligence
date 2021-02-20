84f82c277ba1f11652502ec8df6e4020124ad194
theusgit <theusgit@outlook.com>
2021-02-19 21:09:29 -0300

    Corrigindo erros

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -14,7 +14,7 @@ router.post("/categories/save", (req, res) => {
             title: title,
             slug: slugify(title)
         }).then(() => {
-            res.redirect("/");
+            res.redirect("/admin/categories");
         })
 
     } else {
@@ -24,28 +24,49 @@ router.post("/categories/save", (req, res) => {
 
 router.get("/admin/categories", (req, res) => {
 
-    Category.findAll().then(categories =>{
-        res.render("admin/categories/index",{categories:categories})
+    Category.findAll().then(categories => {
+        res.render("admin/categories/index", { categories: categories })
     });
 });
 
-router.post("/categories/delete",(req,res)=>{
+router.post("/categories/delete", (req, res) => {
     var id = req.body.id;
-    if(id != undefined){
-        if(!isNaN(id)){
+    if (id != undefined) {
+        if (!isNaN(id)) {
             Category.destroy({
                 where: {
-                    id:id
+                    id: id
                 }
-            }).then(()=>{
+            }).then(() => {
                 res.redirect("/admin/categories");
             });
+        } else {
+            res.redirect("/admin/categories");
+        }
+    } else {
+        res.redirect("/admin/categories");
+    }
+});
+
+
+router.get("/admin/categories/edit/:id", (req, res) => {
+    var id = req.params.id;
+
+    if(isNaN(id)){
+        res.redirect("/admin/categories")
+    }
+
+    Category.findByPk(id).then(category => {
+        if(category != undefined){
+         res.render("admin/categories/edit",{category:category})   
+
+
         }else{
             res.redirect("/admin/categories");
         }
-    }else{
+    }).catch(erro=>{
         res.redirect("/admin/categories");
-    }
-})
+    })
+});
 
 module.exports = router;
