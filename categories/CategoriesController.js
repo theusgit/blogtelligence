abe3f2c6522178871e218f1a35c4aa12d22fa807
theusgit <theusgit@outlook.com>
2021-02-23 21:39:23 -0300

    Formulario de artigos

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -52,21 +52,35 @@ router.post("/categories/delete", (req, res) => {
 router.get("/admin/categories/edit/:id", (req, res) => {
     var id = req.params.id;
 
-    if(isNaN(id)){
+    if (isNaN(id)) {
         res.redirect("/admin/categories")
     }
 
     Category.findByPk(id).then(category => {
-        if(category != undefined){
-         res.render("admin/categories/edit",{category:category})   
+        if (category != undefined) {
+            res.render("admin/categories/edit", { category: category })
 
 
-        }else{
+        } else {
             res.redirect("/admin/categories");
         }
-    }).catch(erro=>{
+    }).catch(erro => {
         res.redirect("/admin/categories");
     })
 });
+
+router.post("/categories/update", (req, res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+
+    Category.update({ title: title, slug:slugify(title) }, {
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/categories");
+    })
+});
+
 
 module.exports = router;
