53a735ad0b30ff3ca59c83302de443dda33873b9
theusgit <theusgit@outlook.com>
2021-03-03 18:07:51 -0300

    lista de categorias

--- article/ArticlesController.js
+++ article/ArticlesController.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const Category = require('../categories/Category')
 
 router.get("/articles",(req, res)=>{
     res.send("categorias")
 });
 
 router.get("/admin/articles/new",(req,res)=>{
-    res.render("admin/articles/new")
+    Category.findAll().then(categories=>{
+        res.render("admin/articles/new",{categories:categories})
+    })
 });
 
 module.exports=router;
